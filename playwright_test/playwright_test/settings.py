15bba7216866714dc8441efb15f36f5ad42c9077
Brendan Chen <me@bchen.dev>
2024-04-23 10:58:26 -0700

    disable ROBOTSTXT_OBEY for testing

--- playwright_test/playwright_test/settings.py
+++ playwright_test/playwright_test/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "playwright_test.spiders"
 #USER_AGENT = "playwright_test (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
