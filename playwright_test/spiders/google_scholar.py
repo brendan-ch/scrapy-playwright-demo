04ac5c54a1ca6e72d5562c45d81917432cd7e6a9
Brendan Chen <me@bchen.dev>
2024-05-06 17:20:11 -0700

    add experimental google scholar crawler

---
+++ playwright_test/spiders/google_scholar.py
@@ -0,0 +1,29 @@
+import urllib.parse
+from pathlib import Path
+import scrapy
+from scrapy.settings import BaseSettings
+
+class GoogleScholar(scrapy.Spider):
+    name = "google_scholar"
+    allowed_domains = ["scholar.google.com"]
+
+    def start_requests(self):
+        url = "https://scholar.google.com/scholar?q=scrapy"
+
+        query = getattr(self, "query", None)
+        if query is not None:
+            url = f"https://scholar.google.com/scholar?q={urllib.parse.quote(query)}"
+
+        yield scrapy.Request(url, meta={"playwright": True})
+    
+    def parse(self, response, **kwargs):
+        selectors_containing_links = response.xpath("//div[h3]")
+        headings = selectors_containing_links.xpath("//h3[a]")
+        for heading in headings:
+            # Select the href attribute
+            yield {
+                "href": heading.css("::attr(href)").get(),
+                # TODO: fix text extraction
+                "title": heading.css("::text").get(),
+            }
+        
